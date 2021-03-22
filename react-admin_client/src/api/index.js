f4108238fb063381c393c0e9a8e9369e0b2372df
Bai <bsj980612@163.com>
2021-03-22 19:01:26 +0800

    品类管理请求一级分类列表

--- react-admin_client/src/api/index.js
+++ react-admin_client/src/api/index.js
@@ -15,6 +15,20 @@ export const reqLogin = (username, password) =>
 //添加用户
 export const reqAddUser = (user) => ajax("/manage/user/add", user, "POST");
 
+//获取一级或二级分类列表（一级列表的parentId为"0"，二级列表的parentId为所处一级分类的"_id"）
+export const reqCategorys = (parentId) =>
+  ajax("/manage/category/list", { parentId });
+
+//添加分类
+export const reqAddCategorys = (parentId, categoryName) => {
+  ajax("/manage/category/add", { parentId, categoryName }, "POST");
+};
+
+//更新分类
+export const reqUpdateCategorys = (categoryId, categoryName) => {
+  ajax("/manage/category/update", { categoryId, categoryName }, "POST");
+};
+
 //获取天气
 export async function reqWeather() {
   const ip = await getIpClient();
--- react-admin_client/src/pages/Category/index.jsx
+++ react-admin_client/src/pages/Category/index.jsx
@@ -3,9 +3,100 @@
  */
 
 import React, { Component } from "react";
+import { Card, Table, Button, message } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+
+import { reqAddCategorys, reqCategorys, reqUpdateCategorys } from "../../api";
 
 export default class Category extends Component {
+  state = {
+    categorys: [], //一级分类列表
+  };
+
+  //初始化Table所有列的数组
+  //根据dataIndex选定展示字段
+  initColumns = () => {
+    this.columns = [
+      {
+        title: "分类名称",
+        dataIndex: "name",
+      },
+
+      //渲染固定的字段列
+      {
+        title: "操作",
+        width: 300,
+        render: () => (
+          <span>
+            <Button type="link">修改分类</Button>
+            <Button type="link">查看子分类</Button>
+          </span>
+        ),
+      },
+    ];
+  };
+
+  //异步获取一级分类列表显示
+  getCategorys = async () => {
+    const result = await reqCategorys("0");
+    console.log(result);
+    if (result.status === 0) {
+      const categorys = result.data;
+      //更新状态
+      this.setState({
+        categorys,
+      });
+    } else {
+      message.error("获取分类列表失败");
+    }
+  };
+
+  componentWillMount() {
+    this.initColumns();
+  }
+
+  //发送异步ajax请求
+  componentDidMount() {
+    this.getCategorys();
+  }
+
   render() {
-    return <div>Category</div>;
+    //读取状态中的数据
+    const { categorys } = this.state.categorys;
+
+    const title = "一级分类列表";
+
+    const extra = (
+      <Button type="primary" icon={<PlusOutlined />}>
+        添加
+      </Button>
+    );
+
+    const dataSource = [
+      {
+        key: "1",
+        name: "胡彦斌",
+        age: 32,
+        address: "西湖区湖底公园1号",
+      },
+      {
+        key: "2",
+        name: "胡彦祖",
+        age: 42,
+        address: "西湖区湖底公园1号",
+      },
+    ];
+
+    return (
+      <Card title={title} extra={extra} style={{ width: "100%" }}>
+        <Table
+          bordered
+          rowKey="_id"
+          dataSource={this.state.categorys}
+          columns={this.columns}
+        />
+        ;
+      </Card>
+    );
   }
 }
