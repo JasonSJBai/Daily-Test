fbcf670944397196a4c3dc39e2134e0666f929f7
Bai <bsj980612@163.com>
2021-04-01 10:53:48 +0800

    ..

--- react-admin_client/src/pages/Product/ProductAddUpdate.jsx
+++ react-admin_client/src/pages/Product/ProductAddUpdate.jsx
@@ -3,12 +3,17 @@
  */
 
 import React, { Component } from "react";
-import { Card, Form, Button } from "antd";
+import { Card, Form, Button, Input, Cascader, Upload } from "antd";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 
 const Item = Form.Item;
+const TextArea = Input.TextArea;
 
 export default class ProductAddUpdate extends Component {
+  getFieldsValue = (values) => {
+    console.log(values);
+  };
+
   render() {
     const title = (
       <div>
@@ -18,14 +23,69 @@ export default class ProductAddUpdate extends Component {
           icon={<ArrowLeftOutlined />}
           size="large"
         />
-        <span>新增/修改商品</span>
+        <span>添加商品</span>
       </div>
     );
 
+    //指定item布局的配置对象
+    const FormItemStyle = {
+      labelCol: { span: 2 }, //左侧label
+      wrapperCol: { span: 8 }, //指定右侧包裹的宽度
+    };
+
     return (
       <Card title={title}>
-        <Form>
-          <Item></Item>
+        <Form
+          {...FormItemStyle}
+          onFinish={this.getFieldsValue}
+          initialValues={{
+            productName: "",
+            productDesc: "",
+            productPrice: "",
+            productType: "",
+            // productPhoto: "",
+            productDetial: "",
+          }}
+        >
+          <Item
+            name="productName"
+            label="商品名称"
+            rules={[{ required: true, message: "请输入商品名称" }]}
+          >
+            <Input />
+          </Item>
+          <Item
+            name="productDesc"
+            label="商品描述"
+            rules={[{ required: true, message: "请输入商品描述" }]}
+          >
+            <TextArea autoSize={{ minRows: 2 }} />
+          </Item>
+          <Item
+            name="productPrice"
+            label="商品价格"
+            rules={[{ required: true, message: "请输入价格" }]}
+          >
+            <Input type="number" addonAfter="元" />
+          </Item>
+          <Item
+            name="productType"
+            label="商品分类"
+            rules={[{ required: true, message: "请选择商品分类" }]}
+          >
+            <Cascader></Cascader>
+          </Item>
+          {/* <Item name="productPhoto" label="商品图片">
+            <Upload />
+          </Item> */}
+          <Item name="productDetial" label="商品详情">
+            <TextArea />
+          </Item>
+          <Item>
+            <Button type="primary" htmlType="submit">
+              提交
+            </Button>
+          </Item>
         </Form>
       </Card>
     );
