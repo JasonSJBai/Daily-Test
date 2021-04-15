7398431decb6b21c05c8901c9c7bf316da895b75
Bai <bsj980612@163.com>
2021-04-15 15:59:25 +0800

    级联选择

--- react-admin_client/src/pages/Product/ProductAddUpdate.jsx
+++ react-admin_client/src/pages/Product/ProductAddUpdate.jsx
@@ -5,14 +5,80 @@
 import React, { Component } from "react";
 import { Card, Form, Button, Input, Cascader, Upload } from "antd";
 import { ArrowLeftOutlined } from "@ant-design/icons";
+import { reqCategorys } from "../../api";
 
 const Item = Form.Item;
 const TextArea = Input.TextArea;
 
 export default class ProductAddUpdate extends Component {
+  state = {
+    options: [],
+  };
+
+  initOptions = (categorys) => {
+    //根据categorys生产options数组
+    const options = categorys.map((c) => ({
+      value: c._id,
+      label: c.name,
+      isLeaf: false,
+    }));
+
+    //更新options
+    this.setState({
+      options,
+    });
+  };
+
+  //获取一级/二级分类列表并展示
+  getCategorys = async (parentId) => {
+    const result = await reqCategorys(parentId);
+    if (result.status === 0) {
+      const categorys = result.data;
+      //如果是一级分类列表
+      if (parentId === 0) {
+        this.initOptions(categorys);
+      } else {
+        //二级分类
+        return categorys; //返回二级列表
+      }
+    }
+  };
+
+  //选择非叶子选项时，加载下一级选项数据
+  loadData = async (selectedOptions) => {
+    //得到选择的option对象
+    const targetOption = selectedOptions[0]; //selectedOptions[selectedOptions.length - 1];
+    //显示loading
+    targetOption.loading = true;
+
+    //根据选中的分类请求获取下一级分类列表
+    const subCategorys = await this.getCategorys(targetOption.value);
+    targetOption.loading = false;
+
+    if (subCategorys && subCategorys.length > 0) {
+      //生成一个二级列表的options
+      const childOptions = subCategorys.map((c) => ({
+        value: c._id,
+        label: c.name,
+        isLeaf: true,
+      }));
+      //关联到当前的option上
+      targetOption.children = childOptions;
+    } else {
+      //当前选择的分类没有二级分类
+      targetOption.isLeaf = true;
+    }
+
+    this.setState({ options: [...this.state.options] });
+  };
+
   getFieldsValue = (values) => {
     console.log(values);
   };
+
+  componentDidMount() {
+    this.getCategorys(0);
+  }
 
   render() {
     const title = (
@@ -73,7 +139,7 @@ export default class ProductAddUpdate extends Component {
             label="商品分类"
             rules={[{ required: true, message: "请选择商品分类" }]}
           >
-            <Cascader></Cascader>
+            <Cascader options={this.state.options} loadData={this.loadData} />
           </Item>
           {/* <Item name="productPhoto" label="商品图片">
             <Upload />
